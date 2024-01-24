f263986ecaa28bae5ffa47b2d8ac78a6e26c679d
ASUS <ASUS@LAPTOP-L43S1IA6>
2024-01-24 14:50:59 -0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+ 
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("enter lenth of rectangle");
+                double length = double.Parse(Console.ReadLine());
+                Console.WriteLine("enter width of rectangle");
+                double width = double.Parse(Console.ReadLine());
+                double area = Area(length, width);
+                Console.WriteLine("Area is :" + area);
+
+            }
+            static double Area(double length, double width)
+            {
+                return length * width;
+            }
+        }
+    }
